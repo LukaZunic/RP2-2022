3146b2de40ef1ab3e2489ec393e937e0f43af6eb
LukaZunic <lzunic99@gmail.com>
2022-07-09 21:27:57 +0200

    Details

--- server/getInternships.php
+++ server/getInternships.php
@@ -7,12 +7,34 @@
         die('Could not connect: ' . mysqli_error($conn));
     }
 
-    $sql="SELECT * FROM internship";
-    $result = $conn->query($sql);
-    $internships = array();
-    while($row = $result->fetch()) {
-        $internships[] = $row;
+    $id = $_GET['id'];
+
+    if($id != -1) {
+        $sql="SELECT * FROM internship WHERE ID = $id";
+        $result = $conn->query($sql);
+
+        $internships = array();
+        while($row = $result->fetch()) {
+            $internships[] = $row;
+        }
+    } else {
+        $sql="SELECT * FROM internship";
+        $result = $conn->query($sql);
+
+        $internships = array();
+        while($row = $result->fetch()) {
+            $internships[] = $row;
+        }
     }
+
+    // $sql="SELECT * FROM internship";
+    // $result = $conn->query($sql);
+
+    // $internships = array();
+    // while($row = $result->fetch()) {
+    //     $internships[] = $row;
+    // }
+
     echo json_encode($internships);
 
 ?>
