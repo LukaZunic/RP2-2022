7c39c8357cf59099f193cd0d58702585b2c9587e
LukaZunic <lzunic99@gmail.com>
2022-07-09 19:07:54 +0200

    Cards

--- server/getInternships.php
+++ server/getInternships.php
@@ -1,30 +1,5 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <style>
-        table {
-        width: 100%;
-        border-collapse: collapse;
-        }
-
-        table, td, th {
-        border: 1px solid black;
-        padding: 5px;
-        }
-
-        th {text-align: left;}
-    </style>
-</head>
-<body>
-
 <?php
     include_once 'db.php';
-
-    // $q = intval($_GET['q']);
-
-    // q = all
-
-    // echo $q;
 
     $conn = DB::getConnection();
 
@@ -32,26 +7,12 @@
         die('Could not connect: ' . mysqli_error($conn));
     }
 
-    $sql="SELECT * FROM internships";
-    $result = mysqli_query($conn, $sql);
-
-    // echo "<table>
-    //     <tr>
-    //     <th>id</th>
-    //     <th>title</th>
-    //     <th>company</th>
-    //     <th>description</th>
-    //     </tr>";
-    while($row = mysqli_fetch_array($result)) {
-      echo "<tr>";
-      echo "<td>" . $row['id'] . "</td>";
-      echo "<td>" . $row['title'] . "</td>";
-      echo "<td>" . $row['company'] . "</td>";
-      echo "<td>" . $row['description'] . "</td>";
-      echo "</tr>";
+    $sql="SELECT * FROM internship";
+    $result = $conn->query($sql);
+    $internships = array();
+    while($row = $result->fetch()) {
+        $internships[] = $row;
     }
-    echo "</table>";
-    mysqli_close($conn);
+    echo json_encode($internships);
+
 ?>
-</body>
-</html>
