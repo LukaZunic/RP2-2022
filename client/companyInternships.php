a4fc382b08a828dd0e9858f447cdec7cb72e6f7c
LukaZunic <lzunic99@gmail.com>
2022-07-11 12:31:58 +0200

    Guard

--- client/companyInternships.php
+++ client/companyInternships.php
@@ -2,7 +2,7 @@
     session_start();
 
     if(!isset($_SESSION['user_type'])) {
-      header('login.php');
+      header("Location: ./handleLogin.php");
     }
 
     if(isset($_SESSION['user_type'])) {
